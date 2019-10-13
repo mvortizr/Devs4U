a2a8f85643fe9e9c00a4b80ea22807cd970e9800
Black995 <alansaul25@gmail.com>
2019-10-13 18:35:17 -0400

    Cambios hechos en el dashboard y en el login

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter as  Router, Switch, Route} from "react-router-dom";
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import Login from './pages/Login';
-import Registration from './pages/Registration'
+import DashboardFree from './pages/DashboardFree';
+import Registration from './pages/Registration';
 
 
 /* TODO manejar los roles*/
@@ -14,6 +15,7 @@ function App() {
 	      <Route exact path="/" render={ ()=> <Home/>}/>
 	      <Route exact path='/login' render={ ()=> <Login/>}/>
 	      <Route exact path='/register' render={ ()=> <Registration/>}/>
+		  <Route exact path='/dashboard' render={ ()=> <DashboardFree/>}/>
 	      <Route component={NotFound} />
 	    </Switch>
     </Router>
