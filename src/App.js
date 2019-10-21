9d6779e0e7907f54ca30b574f8f63cb186582642
Black995 <alansaul25@gmail.com>
2019-10-20 21:11:04 -0400

    Dashboards y perfiles

--- src/App.js
+++ src/App.js
@@ -6,18 +6,20 @@ import Login from './pages/Login';
 import DashboardFree from './pages/DashboardFree';
 import DashboardCont from './pages/DashboardCont';
 import Registration from './pages/Registration';
-
+import PerfilFree from './pages/PerfilFree';
+import PerfilCont from './pages/PerfilCont';
 
 /* TODO manejar los roles*/
 function App() {
   return (
     <Router>
 	    <Switch>
-	      <Route exact path="/" render={ ()=> <Home/>}/>
-	      <Route exact path='/login' render={ ()=> <Login/>}/>
+	      <Route exact path="/" render={ ()=> <Login/>}/>
 	      <Route exact path='/register' render={ ()=> <Registration/>}/>
 		  <Route exact path='/dashboardfreelancer' render={ ()=> <DashboardFree/>}/>
 		  <Route exact path='/dashboardcontratista' render={ ()=> <DashboardCont/>}/>
+		  <Route exact path='/perfilfreelancer' render={ ()=> <PerfilFree/>}/>
+		  <Route exact path='/perfilcontratista' render={ ()=> <PerfilCont/>}/>
 	      <Route component={NotFound} />
 	    </Switch>
     </Router>
---
+++ src/pages/PerfilCont.js
@@ -0,0 +1,271 @@
+import React from 'react';
+import clsx from 'clsx';
+import {Typography, Drawer, AppBar, Toolbar, List, Divider, IconButton, Badge, Grid, CssBaseline, Container, Paper, Link, Card, CardActions, CardMedia, CardContent, Button} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import { mainListItems, secondaryListItems } from './ListaItemsCont';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Devs4U
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  toolbar: {
+    paddingRight: 24, // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  menuButtonHidden: {
+    display: 'none',
+  },
+  title: {
+    flexGrow: 1,
+  },
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9),
+    },
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+  fixedHeight: {
+    height: 240,
+  },
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+
+  markdown: {
+    ...theme.typography.body2,
+    padding: theme.spacing(3, 0),
+  },
+}));
+
+const archives = [
+  'Trabajo 1',
+  'Trabajo 2',
+  'Trabajo 3',
+];
+
+
+export default function Dashboard() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
+        <Toolbar className={classes.toolbar}>
+
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+            Perfil
+          </Typography>
+          <IconButton color="inherit">
+            {/*badgeContent muestra la cantidad de notificaciones*/}
+            <Badge badgeContent={0} color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        classes={{
+          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+        }}
+        open={open}
+      >
+        <div className={classes.toolbarIcon}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <List>{mainListItems}</List>
+        <Divider />
+        <List>{secondaryListItems}</List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container className={classes.cardGrid} maxWidth="md">
+
+
+        <Grid container spacing={5} className={classes.mainGrid}>
+            {/* Main content */}
+            <Grid item xs={12} md={8}>
+              <Typography variant="h4" gutterBottom>
+                Nombre del Usuario
+              </Typography>
+              <Divider />
+
+              <Typography variant="h6" gutterBottom>
+                Aquí va imagen
+              </Typography>
+
+              <Typography variant="h6" gutterBottom>
+                Sobre mí:
+              </Typography>
+
+              <Typography paragraph>
+                   XXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                </Typography>
+
+              <Typography variant="h6" gutterBottom>
+                Tipos de trabajos que busco:
+              </Typography>
+
+              <Typography paragraph>
+              <li className={classes.listItem}>
+                    <Typography component="span"/> Holaaaa
+                </li>
+                <li className={classes.listItem}>
+                    <Typography component="span"/> Holis
+              </li>
+              </Typography>
+
+            </Grid>
+            {/* End main content */}
+            {/* Sidebar */}
+            <Grid item xs={12} md={4}>
+              <Paper elevation={0} className={classes.sidebarAboutBox}>
+                <Typography variant="h6" gutterBottom>
+                  Información General
+                </Typography>
+
+                <Typography paragraph>
+                    <strong>Residencia:</strong> Caracas, Venezuela
+                </Typography>
+                <Typography paragraph>
+                    <strong>Empresa:</strong> Oracle
+                </Typography>
+                <Typography paragraph>
+                    <strong>Experiencia:</strong> 10 años
+                </Typography>
+                  
+              </Paper>
+              <Typography variant="h6" gutterBottom className={classes.sidebarSection}>
+                Trabajos contratados y finalizados
+              </Typography>
+              {archives.map(archive => (
+                <Link display="block" variant="body1" href="#" key={archive}>
+                  {archive}
+                </Link>
+              ))}
+
+            </Grid>
+            {/* End sidebar */}
+          </Grid>
+
+        </Container>
+        <Copyright />
+      </main>
+    </div>
+  );
+}
