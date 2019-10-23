641554e86d8260e76860663f69bd3427db6077a9
unknown <hssanchez.17@est.ucab.edu.ve>
2019-10-23 04:33:14 +0200

    Ahora sin estar pegado

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,16 @@
 
 const express = require('express');
 const router = express.Router();
-const axios = require('axios');
+const userController=require('../controllers/UserController');
 
-//Ruta de prueba
+router.post('/register',userController.register);
+router.post('/login',userController.login);
 
-router.get('/prueba', function (req, res) {
-    res.send(JSON.stringify({hello: 'hello'}));
-});
+router.post('/edit',userController.update);
+//app.get('/dashboard',ensureAuthenticated,userController.showDashboard)
+//app.get('/logout',userController.logout)
+
+//app.get('/edit',ensureAuthenticated,userController.edit);
+//app.get('/profile',ensureAuthenticated,userController.show)
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const pg=require('pg');
 const cors = require('cors');
 const path = require ('path');
 const app = express();
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 
 const router = require('./routes/index');
 
 const publicPath = path.join(__dirname, '..', 'public');
 
-
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(publicPath)));
+app.use(cookieSession({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+require('../config/passport')(passport);
+
     
 app.set('PORT', process.env.PORT || 5000);
 
@@ -22,7 +36,7 @@ app.use('/', router);
 //handle all get on index.html
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
-  });
+});
 
 app.listen(app.get('PORT'), () => 
 console.log('Listening at ' + app.get('PORT')))
