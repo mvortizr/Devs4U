3b4e20e0b1c939f88b291087cc516d4a688322af
Maria V <mariatua2@gmail.com>
2019-10-23 21:16:37 -0400

    model portfolio

--- models/portfolioproject.js
+++ models/portfolioproject.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Portfolio = sequelize.define('Portfolio', {
+  const PortfolioProject = sequelize.define('PortfolioProject', {
     name: DataTypes.STRING,
     projectType: DataTypes.STRING,
     linkSee: DataTypes.STRING,
@@ -9,8 +9,8 @@ module.exports = (sequelize, DataTypes) => {
     description: DataTypes.TEXT,
     image: DataTypes.STRING
   }, {});
-  Portfolio.associate = function(models) {
+  PortfolioProject.associate = function(models) {
     // associations can be defined here
   };
-  return Portfolio;
+  return PortfolioProject;
 };
---
+++ server/controllers/ProjectController.js
@@ -0,0 +1,12 @@
+const model=require('../../models');
+module.exports={
+
+   create(req,res){
+      model.User.create({ 
+                    
+    })
+   }
+   
+   
+}
+
